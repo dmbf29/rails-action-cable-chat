6c6b235f3132af6dd7491f581450b3562104a0db
Douglas Berkley <douglasmberkley@gmail.com>
2022-02-27 22:15:49 +0900

    moved style from previous app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Chatroom.destroy_all
+
+puts 'Creating users....'
+User.create!(
+  email: 'douglas.berkley@lewagon.org',
+  password: '123123',
+  nickname: 'dmbf29'
+)
+
+User.create!(
+  email: 'yann.klein@lewagon.org',
+  password: '123123',
+  nickname: 'yannklein'
+)
+
+User.create!(
+  email: 'trouni@lewagon.org',
+  password: '123123',
+  nickname: 'trouni'
+)
+puts "...created #{User.count} users."
+
+puts 'Creating chatrooms....'
+Chatroom.create!(
+  name: 'general'
+)
+
+Chatroom.create!(
+  name: '795'
+)
+puts "...created #{Chatroom.count} chatrooms."
